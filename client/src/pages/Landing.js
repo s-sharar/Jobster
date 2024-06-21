2936d37c8670c94118f64775baacbd69b0a08c36
s-sharar <ssharar@uwaterloo.ca>
2024-06-22 01:06:10 +0600

    Ok2

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -12,15 +12,13 @@ const Landing = () => {
         {/* info */}
         <div className='info'>
           <h1>
-            job <span>tracking</span> app
+            job <span>tracking</span>platform
           </h1>
           <p>
-            Crucifix narwhal street art asymmetrical, humblebrag tote bag pop-up
-            fixie raclette taxidermy craft beer. Brunch bitters synth, VHS
-            crucifix heirloom meggings bicycle rights.
+          Stay organized and boost your job search efficiency with Jobster. Track your applications, set reminders for follow-ups, and manage all your job hunting activities in one place. Our user-friendly interface ensures you never miss an opportunity, keeping you one step ahead in your career journey. With real-time updates and personalized job suggestions, Jobster is your ultimate job search companion.
           </p>
           <Link to='/register' className='btn btn-hero'>
-            Login/Register
+            Login | Register
           </Link>
         </div>
         <img src={main} alt='job hunt' className='img main-img' />
